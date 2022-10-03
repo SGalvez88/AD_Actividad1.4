70f2f8701c47a2062b8f95fe68c93db840ee9f62
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-03 13:36:01 +0200

    Commit: creacion persona

--- src/actividad1/pkg4/Actividad14.java
+++ src/actividad1/pkg4/Actividad14.java
@@ -1,11 +1,25 @@
 
 package actividad1.pkg4;
 
+import classes.DatosPersonales;
+import classes.DatosPublicos;
+import classes.Persona;
+
 public class Actividad14 {
 
     public static void main(String[] args) {
-        System.out.println("Feature/writtingXML");
+        Persona persona = creacionPersona();
         
+        
+        
+    }
+    
+    public static Persona creacionPersona(){
+        DatosPersonales datosPersonales = new DatosPersonales(1, "Galvez");
+        DatosPublicos datosPublicos = new DatosPublicos("INF", 3500);
+        
+        Persona persona = new Persona(datosPersonales, datosPublicos);
+        return persona;
     }
     
 }
