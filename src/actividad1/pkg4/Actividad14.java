7ce9dd8bc135e6b2e2c4a923887cd4f5f8d0ddc3
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-03 13:26:23 +0200

    Commit: creacion rama feature/writtingXML

--- src/actividad1/pkg4/Actividad14.java
+++ src/actividad1/pkg4/Actividad14.java
@@ -4,7 +4,7 @@ package actividad1.pkg4;
 public class Actividad14 {
 
     public static void main(String[] args) {
-        System.out.println("Rama Develop");
+        System.out.println("Feature/writtingXML");
         
     }
     
